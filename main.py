992270cb8877a0db780832d6ba81f2023014c8a3
VictorRodriguez05 <victordanilorodriguezruiz@hotmail.com>
2025-08-07 16:33:03 -0500

    Inicializar proyecto Tarea

---
+++ main.py
@@ -0,0 +1,9 @@
+from task_model import TaskModel
+
+def main():
+    task = TaskModel("Estudiar para el ezamen")
+    print(f"Tarea creada: {task.get_task_name()} ")
+
+
+if __name__=="__main__":
+    main()    
---
+++ task_model.py
@@ -0,0 +1,7 @@
+class TaksModel:
+    def __int__(self, task_name):
+        self.task_name = task_name
+
+    def get_task_name(self):
+        return self.task_name
+        
